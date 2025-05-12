b6ef8db2d182673f216b37b3afc28350977584df
gushint <gushintermeister@gmail.com>
2025-05-11 21:46:51 -0700

    add file

---
+++ install_tools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get update && \
+sudo apt-get install -y \
+	build-essential \
+	git \
+	tmux \
+	python3 \
+	mpv \
+	gcc \
+	gdb \
+	vim \
+	ipython3 \
+	netcat-traditional \
+	wireshark \
+	firefox \
+	strace \
+	ltrace \
+	nmap \
+	checksec \
+	python3-pip \
+	python3-venv \
+	file \
+	ripgrep \
